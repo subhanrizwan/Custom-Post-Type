691e4668a97d530f8d6d260148226dcf7c666dd5
subhanrizwan <subhanahmed444sherazi@gmail.com>
2026-07-30 02:27:41 +0500

    custom post type practice

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-
-function first_post_type() {
+function team_post_type() {
     register_post_type('team', array(
         'labels' => array(
             'name' => 'Team',
             'singular_name' => 'Team Member'
         ),
         'public' => true,
+		
         'supports' => array('title', 'editor', 'thumbnail')
     ));
+    'menu_icon' => 'dashicons-groups',
+	'menu_position' => 20,
 }
 
-add_action('init', 'first_post_type');
+add_action('init', 'team_post_type');
 ?>
