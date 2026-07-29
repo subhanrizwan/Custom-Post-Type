bb0762e05e87ca332c867a1cca1b27bcbcdf1450
subhanrizwan <subhanahmed444sherazi@gmail.com>
2026-07-30 04:24:24 +0500

    update

--- index.php
+++ index.php
@@ -3,14 +3,22 @@ function team_post_type() {
     register_post_type('team', array(
         'labels' => array(
             'name' => 'Team',
-            'singular_name' => 'Team Member'
+            'singular_name' => 'Team Member',
+			'all_items' => 'All Team',
+			'add_new' => 'Add Team',
+			'add_new_item' => 'Add Team'
         ),
         'public' => true,
-		
-        'supports' => array('title', 'editor', 'thumbnail')
+        'menu_icon' => 'dashicons-groups',
+        'menu_position' => 20,
+		'has_archieve' => true,
+        'publicly_queryable' => false,
+        'rewrite' => array(
+            'slug' => 'our-team'
+        ),
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'taxonomies' => array('category', 'post_tag')
     ));
-    'menu_icon' => 'dashicons-groups',
-	'menu_position' => 20,
 }
 
 add_action('init', 'team_post_type');
