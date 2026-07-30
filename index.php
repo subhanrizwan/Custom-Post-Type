19ac5c1e66682bec8aeb35c67ffc65f352404350
subhanrizwan <subhanahmed444sherazi@gmail.com>
2026-07-30 05:46:04 +0500

    update

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 
 // Custom Post Type
-function team_post_type() {
+function team_post_type()
+{
     register_post_type('team', array(
         'labels' => array(
             'name' => 'Team',
             'singular_name' => 'Team Member',
-			'all_items' => 'All Team',
-			'add_new' => 'Add Team',
-			'add_new_item' => 'Add Team'
+            'all_items' => 'All Team',
+            'add_new' => 'Add Team',
+            'add_new_item' => 'Add Team'
         ),
         'public' => true,
         'menu_icon' => 'dashicons-groups',
         'menu_position' => 20,
-		'has_archieve' => true,
-		'publicly_queryable' => false,
-		'show_in_rest' => true,
+        'has_archieve' => true,
+        'publicly_queryable' => false,
+        'show_in_rest' => true,
         'rewrite' => array(
             'slug' => 'our-team'
         ),
@@ -23,6 +24,18 @@ function team_post_type() {
         'taxonomies' => array('category', 'post_tag')
     ));
 }
-
+ 
 add_action('init', 'team_post_type');
-?>
+
+function create_team_taxonomy(){
+	resgiter_taxonomy('designation', 'team', array(
+	
+		'labels' => array(
+		  'name' => 'Designation',
+		  'singular_name' => 'Designation',
+		),
+		'hierarchical'=>true,
+		'public' => true
+	));
+}
+add_action('init', 'create_team_taxonomy');
